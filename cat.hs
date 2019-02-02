9677f9ad4a1907c05f9dfd6d8bf32d594b250386
BeepBop0 <hey.nikhil42@gmail.com>
2019-02-01 19:25:00 -0600

    Replace ByteString with Text since it uses fusions to eliminate intermediary lists and is more space efficient

--- cat.hs
+++ cat.hs
@@ -8,21 +8,10 @@
 -- b = line numbers where blank lines aren't numbered
 -- e = show "ends" (newlines) with "$"
 -- T = show tabs with "^I"
--- TODO: make use of Data.List.Stream for better efficiency
 import System.Environment ( getArgs )
-import qualified Data.ByteString.Lazy as BS
-import qualified Data.ByteString.Lazy.Char8 as C ( 
-    pack, 
-    lines, 
-    unlines, 
-    snoc, 
-    cons, 
-    foldr, 
-    filter, 
-    map, 
-    replicate )
-import Data.Char ( toUpper, toLower )
-import Data.List ( isPrefixOf, genericLength )
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Data.List ( isPrefixOf, genericLength, uncons )
 
 data Result = OK | Error String deriving (Eq)
 data NumMode = B | N deriving (Eq)
@@ -33,8 +22,8 @@ main = do
     cmdargs <- getArgs 
     let f = funcGen dispArgs
         (dispArgs, paths) = filter' ("-" `isPrefixOf`) cmdargs 
-    cat <- foldMap BS.readFile paths                   -- concatenate all of the contents of the files together 
-    BS.putStr $ f cat
+    cat <- foldMap TIO.readFile paths                   -- concatenate all of the contents of the files together 
+    TIO.putStr $ f cat
 
 -- fst = a results in true from f
 -- snd = a results in false from f
@@ -45,19 +34,19 @@ filter' f = foldr g ([], [])
                        else (x, e:y)
 
 -- given a list of options, compose a function that uses all of those functions combined
-funcGen :: [Args] -> (BS.ByteString -> BS.ByteString)
+funcGen :: [Args] -> (T.Text -> T.Text)
 funcGen x 
     | Error x <- hasMeArgs x = error x
-    | otherwise = C.unlines . foldr (\s f -> f . scan s) id x . C.lines
+    | otherwise = T.unlines . foldr (\s f -> f . scan s) id x . T.lines
   where 
-    scan :: Args -> ([BS.ByteString] -> [BS.ByteString])
+    scan :: Args -> ([T.Text] -> [T.Text])
     scan s = case s of
             ('-':o) -> foldr (\c f -> f . func c) id o
             _ -> error "Error: provide arguments in a POSIX manner"
-    func :: Argument -> ([BS.ByteString] -> [BS.ByteString]) 
+    func :: Argument -> ([T.Text] -> [T.Text]) 
     func c = case c of 
-            'u' -> map $ C.map toUpper
-            'l' -> map $ C.map toLower 
+            'u' -> map T.toUpper
+            'l' -> map T.toLower 
             'b' -> lineNum B
             'n' -> lineNum N 
             'e' -> lineEnds 
@@ -83,32 +72,26 @@ hasMeArgs l = case foldr f (Right []) (concat l) of
                     'n' -> 'b'
           meArgs = "ulbn"
 
-showTabs :: BS.ByteString -> BS.ByteString
-showTabs = C.foldr (\c a -> case c of
-                            '\t' -> '^' `C.cons` 'I' `C.cons` a
-                            _ -> c `C.cons` a)
-                 BS.empty
+showTabs :: T.Text -> T.Text
+showTabs = T.replace (T.singleton '\t') (T.pack "^I")
 
-lineEnds :: [BS.ByteString] -> [BS.ByteString]
-lineEnds = map $ flip C.snoc '$'
+lineEnds :: [T.Text] -> [T.Text]
+lineEnds = map $ flip T.snoc '$'
 
 -- if B is enabled, a blank line doesn't include a line number
-lineNum :: NumMode -> ([BS.ByteString] -> [BS.ByteString])
+lineNum :: NumMode -> ([T.Text] -> [T.Text])
 lineNum N = zipWith lineNumFmt [1..]                
 lineNum B = f 1
-  where f :: Int -> [BS.ByteString] -> [BS.ByteString]
+  where f :: Int -> [T.Text] -> [T.Text]
         f _ [] = []
-        f n s = if h == BS.empty
-                   then BS.empty : f n t
-                   else lineNumFmt n h : f (n+1) t
-            where h = head s
-                  t = tail s
+        f n s = if h == T.empty then T.empty : f n t else lineNumFmt n h : f (n+1) t
+          where Just (h, t) = uncons s
                 
-lineNumFmt :: Int -> (BS.ByteString -> BS.ByteString)
-lineNumFmt n l = pad `BS.append` C.pack ns `BS.append` C.pack "  " `BS.append` l
-  where pad = C.replicate (6 - genericLength ns) ' '
+lineNumFmt :: Int -> (T.Text -> T.Text)
+lineNumFmt n l = pad `T.append` T.pack ns `T.append` T.pack "  " `T.append` l
+  where pad = T.replicate (6 - genericLength ns) (T.singleton ' ')
         ns = show n
 
-fmt :: String -> (BS.ByteString -> BS.ByteString)
-fmt ch = C.filter (`notElem` ch)
+fmt :: String -> (T.Text -> T.Text)
+fmt ch = T.filter (`notElem` ch)
 
