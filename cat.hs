d80d03f09e1dc42663701ba8a7e9e3ff44ce1149
BeepBop0 <hey.nikhil42@gmail.com>
2019-02-03 15:11:08 -0600

    Further optimizations: Use text builder to effeciently build Text and use lazy Text to display files larger than system memory. Also include more sensible writing for some functions

--- cat.hs
+++ cat.hs
@@ -9,11 +9,15 @@
 -- e = show "ends" (newlines) with "$"
 -- T = show tabs with "^I"
 import System.Environment ( getArgs )
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.Builder as TB
+import qualified Data.Text.Lazy.IO as TIO
+import Data.Char ( toUpper, toLower )
 import Data.List ( isPrefixOf, genericLength, uncons )
+import Control.Monad ( foldM )
+import Data.Monoid ( (<>) )
 
-data Result = OK | Error String deriving (Eq)
+data Result = OK | Error String deriving (Eq, Show)
 data NumMode = B | N deriving (Eq)
 type Args = String
 type Argument = Char
@@ -29,24 +33,20 @@ main = do
 -- snd = a results in false from f
 filter' :: (a -> Bool) -> [a] -> ([a], [a])
 filter' f = foldr g ([], [])
-  where g e (x,y) = if f e
-                       then (e:x, y)
+  where g e (x,y) = if f e 
+                       then (e:x, y) 
                        else (x, e:y)
 
 -- given a list of options, compose a function that uses all of those functions combined
 funcGen :: [Args] -> (T.Text -> T.Text)
 funcGen x 
     | Error x <- hasMeArgs x = error x
-    | otherwise = T.unlines . foldr (\s f -> f . scan s) id x . T.lines
+    | otherwise = T.unlines . foldr (\s f -> f . func s) id (filter (/= '-') (concat x)) . T.lines
   where 
-    scan :: Args -> ([T.Text] -> [T.Text])
-    scan s = case s of
-            ('-':o) -> foldr (\c f -> f . func c) id o
-            _ -> error "Error: provide arguments in a POSIX manner"
     func :: Argument -> ([T.Text] -> [T.Text]) 
     func c = case c of 
-            'u' -> map T.toUpper
-            'l' -> map T.toLower 
+            'u' -> map $ T.map toUpper
+            'l' -> map $ T.map toLower
             'b' -> lineNum B
             'n' -> lineNum N 
             'e' -> lineEnds 
@@ -55,21 +55,25 @@ funcGen x
             _ -> error "Error: invalid mode. read documentation"
 
 hasMeArgs :: [Args] -> Result 
-hasMeArgs l = case foldr f (Right []) (concat l) of
+hasMeArgs l = case findMeArgs (concat l) of
                 Left x -> Error x
                 _ -> OK
-    where f :: Char -> Either String String -> Either String String
-          f c (Right l) 
-            | c `elem` meArgs = if opp c `elem` l
-                                   then Left $ "Error: mutually exclusive args " ++ show c ++ " and " 
+
+findMeArgs :: String -> Either String String
+findMeArgs = foldM evalStack [] 
+
+evalStack :: String -> Char -> Either String String
+evalStack l c
+    | c `elem` meArgs = if opp c `elem` l
+                           then Left $ "Error: mutually exclusive args " ++ show c ++ " and " 
                                                 ++ (show . opp) c ++ " applied, remove one"
-                                   else Right $ c:l
-          f _ e = e
-          opp c = case c of
-                    'u' -> 'l'
-                    'l' -> 'u'
-                    'b' -> 'n'
-                    'n' -> 'b'
+                           else Right $ c:l
+    | otherwise = Right l
+    where opp c = case c of
+            'u' -> 'l'
+            'l' -> 'u'
+            'b' -> 'n'
+            'n' -> 'b'
           meArgs = "ulbn"
 
 showTabs :: T.Text -> T.Text
@@ -84,13 +88,16 @@ lineNum N = zipWith lineNumFmt [1..]
 lineNum B = f 1
   where f :: Int -> [T.Text] -> [T.Text]
         f _ [] = []
-        f n s = if h == T.empty then T.empty : f n t else lineNumFmt n h : f (n+1) t
+        f n s = if T.null h 
+                   then h : f n t 
+                   else lineNumFmt n h : f (n+1) t
           where Just (h, t) = uncons s
                 
 lineNumFmt :: Int -> (T.Text -> T.Text)
-lineNumFmt n l = pad `T.append` T.pack ns `T.append` T.pack "  " `T.append` l
-  where pad = T.replicate (6 - genericLength ns) (T.singleton ' ')
-        ns = show n
+lineNumFmt n l = TB.toLazyText app <> l
+  where app = TB.fromString pad <> TB.fromString ns <> TB.fromString "  " :: TB.Builder
+        pad = replicate (6 - genericLength ns) ' '
+        ns = show n 
 
 fmt :: String -> (T.Text -> T.Text)
 fmt ch = T.filter (`notElem` ch)
